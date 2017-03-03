ebbf8132d5cc2edb873190c42e4a2e010700bbc9
Nadav Ivgi <nadav@shesek.info>
2017-03-03 22:33:21 +0200

    nicer cli

--- cli.js
+++ cli.js
@@ -1,20 +1,29 @@
 const yargs = require('yargs')
+    , inquirer = require('inquirer')
     , debug = require('debug')('proof-of-hodl')
     , { lock, unlock } = require('./hodl')
 
-    , argv = require('yargs').argv
+require('colors')
 
-//yargs
-  //.usage('$0 <cmd> [args]')
-  //.command('makelock [rlocktime] [msg]', 'make hodl lockbox', { rlocktime: {}, msg: {} }, argv => {
+yargs
+  .usage('$0 <cmd> [args]')
+  .command('lock [msg]', 'create lock proof', { duration: { default: 100 } }, argv => {
+    const lockbox = lock(+argv.duration, argv.msg)
 
-//console.log('argv', argv)
+    //console.log('Ephemeral private key:'.cyan, lockbox.privkey)
+    debug('encumberScript:', lockbox.redeemScript)
+    console.log('Please deposit funds to:'.cyan, lockbox.address)
 
-const lockbox = lock(+argv.rlocktime, argv.msg)
-console.log('lockbox', lockbox)
-
-const coin = { txid: argv.txid, vout: argv.vout, value: argv.value }
-
-const tx = unlock(lockbox, coin, argv.refund_addr)
-
-console.log('tx', tx.toRaw().toString('hex'))
+    inquirer.prompt([
+      { name: 'txid', message: 'txid' }
+    , { name: 'vout', message: 'vout' }
+    , { name: 'value', message: 'value' }
+    , { name: 'refund_addr', message: 'refund addr' }
+    ]).then(answers => {
+      const coin = { txid: answers.txid, vout: +answers.vout, value: +answers.value }
+          , tx = unlock(lockbox, coin, answers.refund_addr)
+      console.log('tx', tx.toRaw().toString('hex'))
+    })
+  })
+  .help()
+  .argv
