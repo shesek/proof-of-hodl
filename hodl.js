3a90e07f3401df5e98beedf162f1cdecaf206f73
Nadav Ivgi <nadav@shesek.info>
2017-03-03 18:38:52 +0200

    set tx version, fix signing of unlock tx (still not properly timelocked)

--- hodl.js
+++ hodl.js
@@ -1,29 +1,36 @@
 const debug = require('debug')('proof-of-hodl')
     , bcoin = require('bcoin')
-    , { script: Script, opcode: Opcode, address: Address, coin: Coin, tx: TX, mtx: MTX } = bcoin
+    , { script: Script, opcode: Opcode, address: Address, coin: Coin, tx: TX, mtx: MTX, bn: BN } = bcoin
     , { PrivateKey } = bcoin.hd
-    , { OP_CHECKSEQUENCEVERIFY, OP_DROP, OP_CHECKSIG } = Script.opcodes
+    , { OP_CHECKSEQUENCEVERIFY, OP_DROP, OP_CHECKSIG, OP_0 } = Script.opcodes
 
 const NETWORK = process.env.NETWORK || 'testnet'
+    , FEE = 10000
 
-const H = x => 123 // @TODO
+const H = x => 123 // @XXX
 
-const makeEncumberScript = (pubkey, rlocktime) => Script.fromArray([
-  rlocktime
-, OP_CHECKSEQUENCEVERIFY
-, OP_DROP
-, pubkey.publicKey
-, OP_CHECKSIG
-])
+const rev = str => str.match(/../g).reverse().join('')
+
+const makeEncumberScript = (pubkey, rlocktime) => {
+  const script = new Script
+  //script.push(new BN(rlocktime))
+  //script.push(OP_CHECKSEQUENCEVERIFY)
+  //script.push(OP_DROP)
+  script.push(pubkey.publicKey)
+  script.push(OP_CHECKSIG)
+  script.compile()
+  return script
+}
 
 const makeUnlockTx = (coin, redeemScript, rlocktime, refundAddr) => TX.fromOptions({
-  inputs: [ { prevout: coin, sequence: rlocktime, script: [ redeemScript.toRaw() ] } ]
-, outputs: [ { address: refundAddr, value: coin.value } ]
+  version: 2
+, inputs: [ { prevout: coin, sequence: rlocktime, script: [ OP_0, redeemScript.toRaw() ] } ]
+, outputs: [ { address: refundAddr, value: coin.value - FEE } ]
 })
 
-const signUnlockTx = (privkey, tx) => {
+const signUnlockTx = (privkey, tx, vin, coin) => {
   const mtx = MTX.fromTX(tx)
-  mtx.sign(privkey)
+  mtx.signInput(vin, coin, privkey)
   return mtx.toTX()
 }
 
@@ -31,25 +38,26 @@ const deriveMsgKey = (key, msg) => key.derive(H(msg))
 
 exports.lock = (rlocktime, msg) => {
   debug('lock(%d, %s)', rlocktime, msg)
-  const privkey = PrivateKey.generate(NETWORK)
-      , mpubkey = deriveMsgKey(privkey, msg).toPublic()
+  const privkey      = PrivateKey.fromSeed(new Buffer('001100110011001100119903')) // generate(NETWORK) // @XXX
+      , mpubkey      = deriveMsgKey(privkey, msg).toPublic()
       , redeemScript = makeEncumberScript(mpubkey, rlocktime)
       , outputScript = Script.fromScripthash(redeemScript.hash160())
-      , address = Address.fromScript(outputScript)
+      , address      = Address.fromScript(outputScript)
 
   return {
     redeemScript: redeemScript.toString()
-  , address: address.toBase58(NETWORK)
-  , privkey: privkey.toBase58(NETWORK)
+  , address:      address.toBase58(NETWORK)
+  , privkey:      privkey.toBase58(NETWORK)
   , msg, rlocktime
   }
 }
 
 exports.unlock = ({ privkey, rlocktime, msg }, { txid, vout, value }, refundAddr) => {
-  const mprivkey = deriveMsgKey(PrivateKey.fromBase58(privkey), msg)
+  const mprivkey     = deriveMsgKey(PrivateKey.fromBase58(privkey), msg)
       , redeemScript = makeEncumberScript(mprivkey.toPublic(), rlocktime)
       , outputScript = Script.fromScripthash(redeemScript.hash160())
-      , coin = Coin.fromOptions({ hash: txid, index: vout, script: outputScript, value })
+      , coin         = Coin.fromOptions({ hash: rev(txid), index: vout, script: outputScript, value })
+  console.log('coin value:',+coin.value,coin.value)
 
-  return signUnlockTx(mprivkey, makeUnlockTx(coin, redeemScript, rlocktime, refundAddr))
+  return signUnlockTx(mprivkey, makeUnlockTx(coin, redeemScript, rlocktime, refundAddr), 0, coin)
 }
