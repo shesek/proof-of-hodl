03f65eae5c618aedd0c608bdd44cbf36803cf16a
Nadav Ivgi <nadav@shesek.info>
2017-03-03 18:57:48 +0200

    show tx as hex

--- cli.js
+++ cli.js
@@ -8,7 +8,7 @@ const yargs = require('yargs')
   //.usage('$0 <cmd> [args]')
   //.command('makelock [rlocktime] [msg]', 'make hodl lockbox', { rlocktime: {}, msg: {} }, argv => {
 
-console.log('argv', argv)
+//console.log('argv', argv)
 
 const lockbox = lock(+argv.rlocktime, argv.msg)
 console.log('lockbox', lockbox)
@@ -17,4 +17,4 @@ const coin = { txid: argv.txid, vout: argv.vout, value: argv.value }
 
 const tx = unlock(lockbox, coin, argv.refund_addr)
 
-console.log('tx', tx)
+console.log('tx', tx.toRaw().toString('hex'))
