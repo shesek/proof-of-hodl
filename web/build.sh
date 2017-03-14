1941d1329f65c29db946f0903141e441c4909b67
Nadav Ivgi <nadav@shesek.info>
2017-03-14 06:44:28 +0200

    use custom bootstrap build to further reduce bundle size (down to 911k)

--- web/build.sh
+++ web/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 (cat static/jquery-3.1.1.slim.min.js \
-     static/bootstrap/js/bootstrap.mini.js; \
+     static/bootstrap/js/bootstrap.custom.min.js; \
  echo ';'; \
  browserify client.js | uglifyjs -c warnings=false -m \
 )  > static/q.js
